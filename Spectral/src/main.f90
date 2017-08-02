e4e5a62c7fa6d6ec89c10c77788069b763e2756a
pbrubeck <brubeck@protonmail.com>
2017-08-02 16:49:51 -0500

    Internship Report

--- Spectral/src/main.f90
+++ Spectral/src/main.f90
@@ -2,6 +2,6 @@ program main
     implicit none
     !call poisson(64,[1.0d0,1.0d0],[1.0d0,-1.0d0],[1.0d0,-1.0d0]);
 
-    call elliptic(128,128);
+    call elliptic(256,256);
     print *, 'Program terminated.'
 end program main
